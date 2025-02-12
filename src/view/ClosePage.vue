e1998942213427497b894f180613094312824030
algazza <fadhilaswadira72564@gmail.com>
2025-02-12 19:12:46 +0700

    :art: Closing pre order

---
+++ src/view/ClosePage.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+
+import titleUnwrapped from "@/assets/SnapanUnwrappedLove 1.png";
+</script>
+
+<template>
+  <section class="flex justify-center items-center w-full flex-col gap-4 h-screen ">
+    <img :src="titleUnwrapped" alt="Snapan Unwrappd Love" class="mx-auto  px-8 sm:max-w-[500px]">
+    <div class="text-center mx-2 sm:px-4">
+      <h1 class="font-bold text-3xl text-red-600">
+        Pemesanan Sudah Ditutup
+      </h1>
+      <p>
+        Terimakasih sudah memesan di osis8_store :D
+      </p>
+
+    </div>
+  </section>
+</template>
+
+<style scoped>
+
+</style>
